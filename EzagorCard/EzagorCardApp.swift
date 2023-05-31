45d4e7e53aa8dd29b3e38c186dcf375e82d25d19
Ata <atakanozalan@gmail.com>
2023-05-31 17:42:14 +0300

    Initial Commit

---
+++ EzagorCard/EzagorCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EzagorCardApp.swift
+//  EzagorCard
+//
+//  Created by Ezagor on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EzagorCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
